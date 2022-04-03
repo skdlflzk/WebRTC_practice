4ce91c89bb060f45a84f6b5331e286890b68b835
skdlflzk <skdlflzk@gmail.com>
2022-04-03 15:33:51 +0900

    모든 js수정할때마다 nodemon이 재시작 -> 불편. nodemon.json에 config를 추가 "ignore":[src/public/*]

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 
 const app = express();
+app.set('view engine', "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req,res)=>res.render("home"));
 
 app.listen(3000);
 
